2e3a40a287406856431410336b12d8f9597fee0c
mdmorandi-prog <mdmorandi@gmail.com>
2026-02-21 15:12:58 -0300

    fix: refactor prisma to use default node_modules output for vercel compatibility

--- test-get-members.ts
+++
@@ -1,9 +0,0 @@
-import { getMembers } from './src/app/comite/membros/actions'
-
-async function run() {
-    console.log("Calling getMembers()...")
-    const result = await getMembers()
-    console.log("Result:", result)
-}
-
-run()
